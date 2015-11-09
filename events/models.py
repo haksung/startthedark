4c5e3b11df23ff5bf27bcb0d41e34037c0b857a3
haksung <hacksungeeda@gmail.com>
2015-11-09 23:36:22 +0900

    3rd chapter

--- events/models.py
+++ events/models.py
@@ -3,6 +3,25 @@ from datetime import datetime, timedelta
 from django.contrib.auth.models import User
 from django.db.models.query import QuerySet
 
+def today():
+    now = datetime.now()
+    start = datetime.min.replace(year=now.year, month=now.month,
+                                day=now.day)
+    end = (start + timedelta(days=1)) - timedelta.resolution
+    return (start, end)
+
+class EventQuerySet(QuerySet):
+    def today(self):
+        return self.filter(creation_date__range=today())
+
+class EventManager(models.Manager):
+    def get_query_set(self):
+        return EventQuerySet(self.model)
+    
+    def today(self):
+        return self.get_query_set().today()
+    
+
 class Event(models.Model):
     description = models.TextField()
     creation_date = models.DateTimeField(default=datetime.now)
@@ -11,16 +30,14 @@ class Event(models.Model):
     attendees = models.ManyToManyField(User, through="Attendance")
     latest = models.BooleanField(default=True)
 
+    objects = EventManager()
+
     def __unicode__(self):
         return self.description
 
     def save(self, **kwargs):
-        now = datetime.now()
-        start = datetime.min.replace(year=now.year, month=now.month,
-                                     day=now.day)
-        end = (start + timedelta(days=1)) - timedelta.resolution
-        Event.objects.filter(latest=True, creator=self.creator).filter(
-            creation_date__range=(start, end)).update(latest=False)
+        Event.objects.today().filter(latest=True,
+                        creator=self.creator).update(latest=False)
         super(Event, self).save(**kwargs)
 
 class Attendance(models.Model):
